8499b1366df5d119ac41c7003cccbed79b80cb40
Felipe Felix <lipegfelix@gmail.com>
2026-05-02 17:18:32 -0300

    Add files via upload

---
+++ virustest.py
@@ -0,0 +1,13 @@
+from tkinter import *
+
+
+while True:
+    janela = Tk()
+    img = PhotoImage(file="virus/Trollface.png")
+    Label(janela, image=img).pack()
+    janela.title("XD LOL TROLL")
+    janela.geometry("500x300")
+
+
+#apenas para testes do código para parar quando necessário:
+#janela.mainloop()
