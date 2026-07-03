bc4bea126dd4e32bb1c1f05d3306c3e5ba364f00
Felipe Felix <lipegfelix@gmail.com>
2026-07-03 20:44:45 -0300

    Add files via upload

---
+++ test1.py
@@ -0,0 +1,16 @@
+import tkinter
+from tkinter import *
+from tkinter import ttk
+
+janela = Tk()
+
+text = ttk.Label(text = "Hello World").place(x= 1)
+janela.title("my first tkinter project!")
+janela.iconbitmap("imagemdofofodoethan.jpg")
+janela.geometry("700x500")
+janela.wm_maxsize(width = 800, height= 600)
+janela.wm_minsize(width = 600, height= 400)
+
+janela.resizable(width= False, height= False)
+
+janela.mainloop()
