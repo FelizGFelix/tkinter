649d3e36401de191cb99cadb75a20bfa573b577c
Felipe Felix <lipegfelix@gmail.com>
2026-07-04 19:20:38 -0300

    Add files via upload

--- test_label.py
+++ test_label.py
@@ -4,5 +4,18 @@ from tkinter import ttk
 
 janela = Tk()
 janela.title("Label")
+janela.geometry("250x250")
+janela.resizable(width=False, height=False)
+
+texto_label = Label(janela, width=10, height=2, text = "hello world!", fg = "green", bg = "red")
+texto_label.grid(row=0, column=0)
+
+texto_label = Label(janela, width=20, height=2, text = "my label test stuff", fg = "blue", bg = "red")
+texto_label.grid(row=20, column=0)
+
+texto_label = Label(janela, width=20, height=2, text = "i hope you guys like :)", fg = "yellow", bg = "red")
+texto_label.grid(row=30, column=0)
+
+janela.configure(background='red')
 
 janela.mainloop()
